ec0815fcf887d76674c08afa14d2b702291882b0
FreeCX <dr.FreeCX@gmail.com>
2014-05-05 16:54:01 +0400

    upd

--- code/plotting/plotter.cpp
+++ code/plotting/plotter.cpp
@@ -2,8 +2,8 @@
 /*   Автоматизированное построение графиков   */
 /* программа: Light Plotter                   */
 /* автор: dr.FreeCX                           */
-/* релиз: 20/04/2014                          */
-/* версия: 0.5                                */
+/* релиз: 04/05/2014                          */
+/* версия: 0.6                                */
 /* ------------------------------------------ */
 #include <cmath>
 #include <complex>
@@ -20,10 +20,67 @@ enum {
     PLOT_PNG
 };
 
-int Nx, Ny, step;
+int Nx, Ny, center, step;
 double **data;
+double **band;
 const char *fieldB_file = "field_B.txt";
 const char *fieldF_file = "field_F%d.txt";
+
+void plot_profile( int format )
+{
+    FILE *p;
+    char f_format[16];
+
+    p = popen( "gnuplot -p", "w" );
+    fprintf( p, "set size square 1.0,1.0\n" );
+    switch ( format ) {
+        case PLOT_PDF:
+            fprintf( p, "set terminal pdf size 5.0in,5.0in\n" );
+            strcpy( f_format, "pdf" );
+            break;
+        case PLOT_PNG:
+            fprintf( p, "set terminal png size 800,800\n" );
+            strcpy( f_format, "png" );
+            break;
+    }
+    fprintf( p, "set output 'band_profile.%s'\n", f_format );
+    printf( "  >> Plot profile file to 'band_profile' .... " );
+    fprintf( p, "plot '01.txt' w l t 'band 1', '02.txt' w l t 'band 2',"
+                "'03.txt' w l t 'field B'\n" );
+    puts( "[ Done ]" );
+    pclose( p );
+}
+
+void create_profile( void )
+{
+    FILE *f1, *f2, *f3;
+    size_t i, j;
+    double data;
+
+    f1 = fopen( "dataB.dat", "r" );
+    for ( j = 0; j <= Ny; j++ ) {
+        for ( i = 0; i <= Nx; i++ ) {
+            fscanf( f1, "%lf", &data );
+            if ( i == center ) {
+                band[0][j] = data;
+            }
+        }
+    }
+    fclose( f1 );
+    printf( "  >> Create profile files .... " );
+    f1 = fopen( "01.txt", "w" );
+    f2 = fopen( "02.txt", "w" );
+    f3 = fopen( "03.txt", "w" );
+    for ( i = 0; i <= Ny; i++ ) {
+        fprintf( f1, "%d %lf\n", i, band[1][i] ); // band 1
+        fprintf( f2, "%d %lf\n", i, band[2][i] ); // band 2
+        fprintf( f3, "%d %lf\n", i, band[0][i] ); // field B
+    }
+    fclose( f3 );
+    fclose( f2 );
+    fclose( f1 );
+    puts( "[ Done ]" );
+}
 
 void create_plot( const char *input, const char *output, int type, int format )
 {
@@ -89,6 +146,9 @@ int convert_fplot( int N, const char *output_file )
             fscanf( f1, "%lf", &y );
             tmp = {x, y};
             data[j][i] = std::abs( tmp );
+            if ( i == center ) {
+                band[N][j] = data[j][i];
+            }
         }
     }
     puts( "[ Done ]" );
@@ -137,14 +197,14 @@ void main_procedure( int output_format )
     create_plot( "dataB.dat", "field_B", PLOT_3D, output_format );
     create_plot( "dataB.dat", "field_B", PLOT_MAP, output_format );
     /* не обязательный блок */
-    create_plot( "dataT.dat", "field_T", PLOT_3D, output_format );
-    create_plot( "dataT.dat", "field_T", PLOT_MAP, output_format );
-    create_plot( "dataW.dat", "field_W", PLOT_3D, output_format );
-    create_plot( "dataW.dat", "field_W", PLOT_MAP, output_format );
-    create_plot( "dataAx.dat", "field_Ax", PLOT_3D, output_format );
-    create_plot( "dataAx.dat", "field_Ax", PLOT_MAP, output_format );
-    create_plot( "dataAy.dat", "field_Ay", PLOT_3D, output_format );
-    create_plot( "dataAy.dat", "field_Ay", PLOT_MAP, output_format );
+    // create_plot( "dataT.dat", "field_T", PLOT_3D, output_format );
+    // create_plot( "dataT.dat", "field_T", PLOT_MAP, output_format );
+    // create_plot( "dataW.dat", "field_W", PLOT_3D, output_format );
+    // create_plot( "dataW.dat", "field_W", PLOT_MAP, output_format );
+    // create_plot( "dataAx.dat", "field_Ax", PLOT_3D, output_format );
+    // create_plot( "dataAx.dat", "field_Ax", PLOT_MAP, output_format );
+    // create_plot( "dataAy.dat", "field_Ay", PLOT_3D, output_format );
+    // create_plot( "dataAy.dat", "field_Ay", PLOT_MAP, output_format );
     /* конец блока */
     for ( int i = 1; i <= 2; i++ ) {
         sprintf( input, fieldF_file, i );
@@ -153,6 +213,8 @@ void main_procedure( int output_format )
         create_plot( input, output, PLOT_3D, output_format );
         create_plot( input, output, PLOT_MAP, output_format );
     }
+    create_profile();
+    plot_profile( output_format );
     clean_folder();
 }
 
@@ -191,8 +253,12 @@ int folder_scan( int output_format )
 void init_memory( void )
 {
     data = (double **) malloc( (Nx+1) * sizeof(double *) );
+    band = (double **) malloc( 3 * sizeof(double *) );
     for ( int i = 0; i <= Nx; i++ ) {
         data[i] = (double *) malloc( (Ny+1) * sizeof(double) );
+        if ( i < 3 ) {
+            band[i] = (double *) malloc( (Ny+1) * sizeof(double) );
+        }
     }
 }
 
@@ -200,13 +266,18 @@ void clean_memory( void )
 {
     for ( int i = 0; i <= Nx; i++ ) {
         free( data[i] );
+        if ( i < 3 ) {
+            free( band[i] );
+        }
     }
+    free( band );
     free( data );
 }
 
 int main( void )
 {
     Nx = Ny = 1200;
+    center = Ny / 2 - 1;
     step = 1;
     init_memory();
     folder_scan( PLOT_PNG );
